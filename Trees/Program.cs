fa97ef4aadfbf62ac37c8ff6f1da3701455436dd
Jose Miralles <jemiralleslopez@gmail.com>
2019-10-22 01:58:15 +0100

    Add project files.

---
+++ Trees/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bstTest = new Bst<int>();
+            bstTest.Insert(28);
+            bstTest.Insert(37);
+            bstTest.Insert(46);
+            bstTest.Insert(15);
+            bstTest.Insert(12);
+            bstTest.Insert(20);
+
+            foreach (var i in bstTest.TraverseInOrder())
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(bstTest.Min());
+            Console.WriteLine(bstTest.Max());
+            Console.WriteLine(bstTest.Get(20).Value);
+            Console.Read();
+        }
+    }
+}
